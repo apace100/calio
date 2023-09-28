ffe02fb1827ef651d9c8ee8de5aaec6a261a98d3
eggohito <66972816+eggohito@users.noreply.github.com>
2023-09-29 05:34:53 +0800

    Fixed potential issue with serializing to JSON and did some tweaks

--- src/main/java/io/github/apace100/calio/data/SerializableData.java
+++ src/main/java/io/github/apace100/calio/data/SerializableData.java
@@ -1,8 +1,6 @@
 package io.github.apace100.calio.data;
 
 import com.google.common.collect.ImmutableSet;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonSyntaxException;
 import net.minecraft.entity.attribute.EntityAttributeModifier;
@@ -14,6 +12,7 @@ import java.util.LinkedHashMap;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
+@SuppressWarnings("unused")
 public class SerializableData {
 
     // Should be set to the current namespace of the file that is being read. Allows using * in identifiers.
@@ -42,13 +41,16 @@ public class SerializableData {
     public void write(PacketByteBuf buffer, Instance instance) {
         dataFields.forEach((name, field) -> {
             try {
+
                 boolean isPresent = instance.get(name) != null;
-                if(field.hasDefault && field.defaultValue == null) {
+                if (field.hasDefault && field.defaultValue == null) {
                     buffer.writeBoolean(isPresent);
                 }
-                if(isPresent) {
+
+                if (isPresent) {
                     field.dataType.send(buffer, instance.get(name));
                 }
+
             } catch(DataException e) {
                 throw e.prepend(name);
             } catch(Exception e) {
@@ -58,59 +60,70 @@ public class SerializableData {
     }
 
     public JsonObject write(Instance instance) {
+
         JsonObject jsonObject = new JsonObject();
-        dataFields.forEach((name, field) -> {
-            if (instance.get(name) != null) {
-                jsonObject.add(name, field.dataType.write(field));
-            }
-        });
+        dataFields.forEach((name, field) -> instance.ifPresent(name, o -> jsonObject.add(name, field.dataType.write(o))));
+
         return jsonObject;
+
     }
 
     public Instance read(PacketByteBuf buffer) {
+
         Instance instance = new Instance();
         dataFields.forEach((name, field) -> {
             try {
+
                 boolean isPresent = true;
-                if(field.hasDefault && field.defaultValue == null) {
+                if (field.hasDefault && field.defaultValue == null) {
                     isPresent = buffer.readBoolean();
                 }
+
                 instance.set(name, isPresent ? field.dataType.receive(buffer) : null);
+
             } catch (DataException e) {
                 throw e.prepend(name);
             } catch (Exception e) {
                 throw new DataException(DataException.Phase.RECEIVING, name, e);
             }
         });
+
         return instance;
+
     }
 
     public Instance read(JsonObject jsonObject) {
+
         Instance instance = new Instance();
         dataFields.forEach((name, field) -> {
             try {
-                if (!jsonObject.has(name)) {
-                    if (field.hasDefault()) {
-                        instance.set(name, field.getDefault(instance));
-                    } else {
-                        throw new JsonSyntaxException("JSON requires field: " + name);
-                    }
-                } else {
+
+                if (jsonObject.has(name)) {
                     instance.set(name, field.dataType.read(jsonObject.get(name)));
+                } else if (field.hasDefault) {
+                    instance.set(name, field.getDefault(instance));
+                } else {
+                    throw new JsonSyntaxException("JSON requires field: " + name);
                 }
+
             } catch (DataException e) {
                 throw e.prepend(name);
             } catch (Exception e) {
                 throw new DataException(DataException.Phase.READING, name, e);
             }
         });
+
         return instance;
+
     }
 
     public SerializableData copy() {
+
         SerializableData copy = new SerializableData();
         copy.dataFields.putAll(dataFields);
+
         return copy;
+
     }
 
     public Iterable<String> getFieldNames() {
@@ -126,27 +139,27 @@ public class SerializableData {
     }
 
     public class Instance {
-        private HashMap<String, Object> data = new HashMap<>();
 
-        public Instance() {
-
-        }
+        private final HashMap<String, Object> data = new HashMap<>();
 
         public boolean isPresent(String name) {
-            if(dataFields.containsKey(name)) {
-                Field<?> field = dataFields.get(name);
-                if(field.hasDefault && field.defaultValue == null) {
-                    return get(name) != null;
-                }
+
+            if (!dataFields.containsKey(name)) {
+                return data.containsKey(name);
             }
-            return data.containsKey(name);
+
+            Field<?> field = dataFields.get(name);
+            if (field.hasDefault && field.defaultValue == null) {
+                return get(name) != null;
+            }
+
+            return false;
+
         }
 
         public <T> void ifPresent(String name, Consumer<T> consumer) {
-            if(isPresent(name)) {
-                Object o = get(name);
-                T t = (T)o;
-                consumer.accept(t);
+            if (isPresent(name)) {
+                consumer.accept(get(name));
             }
         }
 
@@ -154,40 +167,44 @@ public class SerializableData {
             this.data.put(name, value);
         }
 
+        @SuppressWarnings("unchecked")
         public <T> T get(String name) {
+
             if(!data.containsKey(name)) {
                 throw new RuntimeException("Tried to get field \"" + name + "\" from data, which did not exist.");
             }
-            return (T)data.get(name);
+
+            return (T) data.get(name);
         }
 
         public int getInt(String name) {
-            return (int)get(name);
+            return get(name);
         }
 
         public boolean getBoolean(String name) {
-            return (boolean)get(name);
+            return get(name);
         }
 
         public float getFloat(String name) {
-            return (float)get(name);
+            return get(name);
         }
 
         public double getDouble(String name) {
-            return (double)get(name);
+            return get(name);
         }
 
         public String getString(String name) {
-            return (String)get(name);
+            return get(name);
         }
 
         public Identifier getId(String name) {
-            return (Identifier)get(name);
+            return get(name);
         }
 
         public EntityAttributeModifier getModifier(String name) {
-            return (EntityAttributeModifier)get(name);
+            return get(name);
         }
+
     }
 
     public static class Field<T> {
@@ -226,12 +243,12 @@ public class SerializableData {
         }
 
         public T getDefault(Instance dataInstance) {
-            if(hasDefaultFunction) {
+            if (hasDefaultFunction && defaultFunction != null) {
                 return defaultFunction.apply(dataInstance);
-            } else if(hasDefault) {
+            } else if (hasDefault) {
                 return defaultValue;
             } else {
-                throw new IllegalStateException("Tried to access default value of serializable data entry, when no default was provided.");
+                throw new IllegalArgumentException("Tried to access default value of serializable data entry, when no default was provided");
             }
         }
 
